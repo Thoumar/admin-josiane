ef3eb37e097e9f5912da17c1c28aeb382be2e8c1
Thoumar <thomas@thoumar.com>
2021-02-16 01:04:58 +0100

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a05c002f02d16557a503bf9cf9ee9c29'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e6b93714-0a39-47cd-9ceb-fafbb6cc5afe'
+};
